58f2b1ff892285b2bef74a511abdea11a85936c9
Brett <brett.a.ludwig@gmail.com>
2025-09-07 14:15:39 -0400

    tech-debt-and-updates

---
+++ subdomains/www/app/main.py
@@ -0,0 +1,146 @@
+from fastapi import FastAPI, Request, Response
+from fastapi.templating import Jinja2Templates
+from fastapi.responses import HTMLResponse
+from fastapi.middleware.cors import CORSMiddleware
+from fastapi.staticfiles import StaticFiles
+from collections import OrderedDict
+
+from starlette.middleware.base import BaseHTTPMiddleware, RequestResponseEndpoint
+import requests
+from app.routers import ai_router, writeup_router, runescape_router, squash_the_creeps_router
+
+
+
+origins = [
+    "http://www.dumbfucks.club",
+    "http://redyard:50002",
+]
+CSP: dict[str, str | list[str]] = {
+    "default-src": "'self'",
+    "img-src": [
+        "https://www.dumbfucks.club",
+        "http://redyard:50002",
+    ],
+    "script-src": [
+        "'wasm-unsafe-eval'",
+        "https://cdn.jsdelivr.net/npm/htmx.org@2.0.6/dist/htmx.min.js",
+        "sha384-Akqfrbj/HpNVo8k11SXBb6TlBWmXXlYQrCSqEWmyKJe+hDm3Z/B2WVG4smwBkRVm",
+        "https://www.dumbfucks.club",
+        "http://redyard:50002",
+    ],
+    "style-src": [
+        "https://www.dumbfucks.club",
+        "http://redyard:50002",
+    ],
+    "script-src-elem": [
+        "https://www.dumbfucks.club",
+        "https://cdn.jsdelivr.net/npm/htmx.org@2.0.6/dist/htmx.min.js",
+        "sha384-Akqfrbj/HpNVo8k11SXBb6TlBWmXXlYQrCSqEWmyKJe+hDm3Z/B2WVG4smwBkRVm",
+        "http://redyard:50002",
+    ],
+}
+
+local = True
+if local:
+    origins.append("localhost")
+    for k in CSP:
+        if k != "default-src":
+            CSP[k].append("localhost:8000")
+
+
+
+class SecurityHeadersMiddleware(BaseHTTPMiddleware):
+    """Add security headers to all responses."""
+
+    def __init__(self, app: FastAPI, csp: bool = True) -> None:
+        """Init SecurityHeadersMiddleware.
+
+        :param app: FastAPI instance
+        :param no_csp: If no CSP should be used;
+            defaults to :py:obj:`False`
+        """
+        super().__init__(app)
+        self.csp = csp
+        
+    def parse_policy(self, policy: dict[str, str | list[str]] | str) -> str:
+        """Parse a given policy dict to string."""
+        if isinstance(policy, str):
+            # parse the string into a policy dict
+            policy_string = policy
+            policy = OrderedDict()
+    
+            for policy_part in policy_string.split(";"):
+                policy_parts = policy_part.strip().split(" ")
+                policy[policy_parts[0]] = " ".join(policy_parts[1:])
+    
+        policies = []
+        for section, content in policy.items():
+            if not isinstance(content, str):
+                content = " ".join(content)
+            policy_part = f"{section} {content}"
+    
+            policies.append(policy_part)
+    
+        parsed_policy = "; ".join(policies)
+    
+        return parsed_policy
+
+    async def dispatch(self, request: Request, call_next: RequestResponseEndpoint) -> Response:
+        """Dispatch of the middleware.
+
+        :param request: Incoming request
+        :param call_next: Function to process the request
+        :return: Return response coming from from processed request
+        """
+        headers = {
+            "Content-Security-Policy": "" if not self.csp else self.parse_policy(CSP),
+            "Cross-Origin-Opener-Policy": "same-origin",
+            "Cross-Origin-Embedder-Policy": "require-corp",
+            "Referrer-Policy": "strict-origin-when-cross-origin",
+            "Strict-Transport-Security": "max-age=31556926; includeSubDomains",
+            "X-Content-Type-Options": "nosniff",
+            "X-Frame-Options": "DENY",
+            "X-XSS-Protection": "1; mode=block",
+        }
+        response = await call_next(request)
+        response.headers.update(headers)
+
+        return response
+
+
+app = FastAPI()
+templates = Jinja2Templates(directory="templates")
+app.mount("/static", StaticFiles(directory="static"), name="static")
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=False,
+    allow_methods=["*"],
+    allow_headers=["*"]
+)
+app.add_middleware(SecurityHeadersMiddleware, csp=True)
+app.include_router(ai_router.router)
+app.include_router(writeup_router.router)
+app.include_router(runescape_router.router)
+app.include_router(squash_the_creeps_router.router)
+
+@app.get("/", response_class=HTMLResponse)
+async def home(request: Request):
+    return templates.TemplateResponse("index/index.html", {"request": request} )
+
+@app.get("/cypress", response_class=HTMLResponse)
+async def cypress(request: Request, keyframe):
+    keyframe = int(keyframe) + 1
+    if(keyframe == 7):
+        keyframe = 1
+
+    img_path = "assets/img/Cypress_anim%d.png" % keyframe
+    print(img_path)
+    return templates.TemplateResponse("index/cypress_animation.html", {"request": request, "img_path":img_path, "keyframe":keyframe} )
+
+    
+
+@app.get("/dnd", response_class=HTMLResponse)
+async def dnd(request: Request):
+    return templates.TemplateResponse("portfolio/dnd.html", {"request": request} )
+
---
+++ subdomains/www/static/assets/js/index.js
@@ -0,0 +1,15 @@
+ // Define the function to be called repeatedly
+function updateCypress() {
+    let cypress = document.getElementById("cypress-animation")
+    let keyframe = Number(cypress.getAttribute("keyframe")) + 1;
+    if(keyframe == 10){
+        keyframe = 1
+    }
+    let src = cypress.getAttribute("src");
+    src = src.substring(0, src.length - 5) + keyframe + ".png";
+    cypress.setAttribute("src", src);
+    cypress.setAttribute("keyframe", keyframe);
+  }
+  
+  // Call the function every 2000ms (2 seconds)
+  let intervalId = setInterval(updateCypress, 250);
